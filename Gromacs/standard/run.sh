da276b1886652058322b0db6eb069b6bc4b6eed2
tabrischunchan <tabrischunchan@gmail.com>
2018-10-17 10:42:42 -0400

    modified gromacs run.sh

--- Gromacs/standard/run.sh
+++ Gromacs/standard/run.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
 mkdir -p output
+mini1=false
+mini2=false
+heat=false
+eq_npt=false
+md1=true
+md2=false
 
-if true; then
+if $mini1; then
     prefix=step1_mini_crystal
     gmx grompp -f $prefix.mdp -o $prefix.tpr -c pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
     gmx mdrun -ntomp 8 -v -s $prefix.tpr -deffnm output/$prefix
 fi
     
-if true; then
+if $mini2; then
     prefix1=step1_mini_crystal
     prefix2=step2_mini
     gmx_d grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
     gmx_d mdrun -ntomp 8 -v -s $prefix2.tpr -deffnm output/$prefix2
 fi
 
-if true; then
+if $heat; then
     prefix1=step2_mini
     prefix2=step3_annealing
     gmx grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
     gmx mdrun -ntomp 8 -v -s $prefix2.tpr -deffnm output/$prefix2
 fi
 
-if true; then
+if $eq_npt; then
     prefix1=step3_annealing
     prefix2=step4_eq_npt
     gmx grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
     gmx mdrun -ntomp 8 -v -s $prefix2.tpr -deffnm output/$prefix2
 fi
 
-if true; then
+if $md1; then
     prefix1=step4_eq_npt
     prefix2=step5_md
     gmx grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
     gmx_cuda mdrun -ntomp 8 -v -s $prefix2.tpr -deffnm output/$prefix2
 fi
 
-if false; then
+if $md2; then
     prefix1=step5_md
     prefix2=step5_md-2
     gmx convert-tpr -s $prefix1.tpr -o $prefix2.tpr -extend 1000
