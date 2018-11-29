06b3fe39904e4ca5d008e57b3ba712ee8803402b
Kevin <ukevi@gmx.com>
2018-11-29 15:38:03 -0500

    Update prepare.sh

--- Gromacs/standard/prepare.sh
+++ Gromacs/standard/prepare.sh
@@ -1,7 +1,9 @@
-# Create a cubic box of 2 nm on each side and place the system to the center
+# Create a cubic box of 2 nm on each side and place the system to the center and align its principle axes to the reference axes
+# -princ usually helps to reduce your system size
 gmx editconf -f conf.gro -o editconf.gro -bt cubic -d 2 -c -princ
 # Solvate the box
 gmx solvate -cp editconf.gro -o solvate.gro -p topol.top
-# Add ions to make it neutral and of 0.15M
+# Add ions to make it neutral and of 0.15 M NaCl
+# If you want KCl, add -pname K
 gmx grompp -f ions.mdp -c solvate.gro -o ions.tpr -p topol.top
 gmx genion -s ions.tpr -o ionized.gro -conc 0.15 -neutral -p topol.top
