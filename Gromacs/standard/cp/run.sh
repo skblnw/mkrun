8d7e9052f069ca7d13b7ca0a2a5edf4ab655debc
tabrischunchan <tabrischunchan@gmail.com>
2019-10-20 20:14:14 -0400

    Routine

--- Gromacs/standard/cp/run.sh
+++ Gromacs/standard/cp/run.sh
@@ -11,45 +11,45 @@ md2=false
 if $mini1; then
     prefix=step1_mini_crystal
     rm -f $prefix.tpr
-    gmx grompp -f $prefix.mdp -o $prefix.tpr -c pdb2gmx/ionized.pdb -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
-    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -s $prefix.tpr -deffnm output/$prefix
+    gmx grompp -f $prefix.mdp -o output/$prefix.tpr -c pdb2gmx/ionized.pdb -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
+    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -deffnm output/$prefix
 fi
     
 if $mini2; then
     prefix1=step1_mini_crystal
     prefix2=step2_mini
     rm -f $prefix2.tpr
-    gmx_d grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
-    gmx_d mdrun -pin on -ntmpi 1 -ntomp 12 -v -s $prefix2.tpr -deffnm output/$prefix2
+    gmx_d grompp -f $prefix2.mdp -o output/$prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
+    gmx_d mdrun -pin on -ntmpi 1 -ntomp 12 -v -deffnm output/$prefix2
 fi
 
 if $heat; then
     prefix1=step2_mini
     prefix2=step3_annealing
     rm -f $prefix2.tpr
-    gmx grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
-    gmx mdrun -pin on  -ntmpi 1 -ntomp 12 -v -s $prefix2.tpr -deffnm output/$prefix2
+    gmx grompp -f $prefix2.mdp -o output/$prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
+    gmx mdrun -pin on  -ntmpi 1 -ntomp 12 -v -deffnm output/$prefix2
 fi
 
 if $eq_npt; then
     prefix1=step3_annealing
     prefix2=step4_eq_npt
     rm -f $prefix2.tpr
-    gmx grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
-    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -s $prefix2.tpr -deffnm output/$prefix2
+    gmx grompp -f $prefix2.mdp -o output/$prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
+    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -deffnm output/$prefix2
 fi
 
 if $md1; then
     prefix1=step4_eq_npt
     prefix2=step5_md-1
     rm -f $prefix2.tpr
-    gmx grompp -f $prefix2.mdp -o $prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
-    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -s $prefix2.tpr -deffnm output/$prefix2
+    gmx grompp -f $prefix2.mdp -o output/$prefix2.tpr -c output/$prefix1.gro -r pdb2gmx/ionized.pdb -n pdb2gmx/index.ndx -p pdb2gmx/topol.top
+    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -deffnm output/$prefix2
 fi
 
 if $md2; then
     prefix1=step5_md-1
     prefix2=step5_md-2
 #    gmx convert-tpr -s $prefix1.tpr -o $prefix2.tpr -extend 10000
-    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -noappend -s step5_md-1.tpr -cpi output/$prefix1.cpt -deffnm output/$prefix2 -nsteps -1
+    gmx mdrun -pin on -ntmpi 1 -ntomp 12 -v -noappend -s output/step5_md-1.tpr -cpi output/$prefix1.cpt -deffnm output/$prefix2 -nsteps -1
 fi
--- Gromacs/standard/prepare.sh
+++ Gromacs/standard/prepare.sh
@@ -1,6 +1,6 @@
 # Create a cubic box of 2 nm on each side and place the system to the center and align its principle axes to the reference axes
 # -princ usually helps to reduce your system size
-gmx editconf -f conf.gro -o editconf.pdb -d 2 -c -princ
+gmx editconf -f conf.gro -o editconf.pdb -d 2 -princ
 # Solvate the box
 gmx solvate -cp editconf.pdb -o solvate.gro -p topol.top
 # Add ions to make it neutral and of 0.15 M NaCl
