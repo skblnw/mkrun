17c1c81eec169053614bc008e12495f84f9fca65
skblnw <cchan2242-c@my.cityu.edu.hk>
2020-07-06 11:21:40 -0400

    ROutine

--- Benchmark/Gromacs/grep.sh
+++ Benchmark/Gromacs/grep.sh
@@ -5,6 +5,6 @@ rm -f $filename
 touch $filename
 for ii in {1,2,3,4,8,10}
 do
-NS=`grep -i "Performance" $ii.log | awk '{print $2}'`
-echo -e "$ii\t$NS" >> $filename
+    NS=`grep -i "Performance" $ii.log | awk '{print $2}'`
+    echo -e "$ii\t$NS" >> $filename
 done
