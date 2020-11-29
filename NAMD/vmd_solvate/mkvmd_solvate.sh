d837c90bc2a9884d3bf887b87681fc2ea4f45f30
skblnw <i@skblnw.com>
2020-11-28 23:14:47 -0500

    NAMD solvate

---
+++ NAMD/vmd_solvate/mkvmd_solvate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ~/.zshrc
+
+cat > tcl <<'EOF'
+set filename 6xra_charmmgui
+package require solvate
+#solvate $filename.psf $filename.pdb -b 2.4 -minmax {{-77 -76 -35} {77 76 130}} -o solvated
+solvate $filename.psf $filename.pdb -b 2.4 -t 8 -o solvated
+package require autoionize
+autoionize -psf solvated.psf -pdb solvated.pdb -sc 0.18 -cation POT -o ionized
+quit
+EOF
+
+vmd -dispdev text -e tcl
+rm -f tcl solvated.p*
