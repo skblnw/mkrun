576ba2d31c1733d8414078df3854f859fef9cf95
skblnw <i@skblnw.com>
2021-03-05 08:36:36 +0800

    MDFF solvation

--- NAMD/MD_MDFF/mkvmd_mdff_prepare_solvate.sh
+++ NAMD/MD_MDFF/mkvmd_mdff_prepare_solvate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Must have:
-# 1. prot_charmmgui.psf
-# 2. prot_charmmgui.pdb
+# 1. prot.psf
+# 2. prot.pdb
 # 3. map.mrc
 
 source ~/.zshrc
@@ -37,11 +37,17 @@ set amt [list [expr int([lindex \$min 0])] [expr int([lindex \$min 1])] [expr in
 voltool crop -amt \$amt -i tmp.dx -o ${PREFIX}_grid.dx
 
 mdff setup -o mdff -psf $PSF -pdb $PDB -griddx ${PREFIX}_grid.dx -gridpdb ${PREFIX}_grid.pdb -extrab {${PREFIX}_ssrestraints.txt ${PREFIX}_cispeptide.txt ${PREFIX}_chirality.txt} \\
--gscale 0.1 -minsteps 5000 -numsteps 10000
+-gscale 0.1 -minsteps 5000 -numsteps 10000 -pbc
 
 quit
 EOF
 
 vmd -dispdev text -e tmp.tcl 
+
+cp mdff-step1.namd mdff-step1.namd.BAK
+
+sed -e "s/parameters par_all36_prot.prm//g" \
+-e "s/paraTypeCharmm on/paraTypeCharmm on\nparameters par_all36m_prot.prm\nmergeCrossterms yes\nparameters par_all36_lipid.prm\nparameters par_all36_carb.prm\nparameters par_all36_na.prm\nparameters par_all36_cgenff.prm\nparameters toppar_water_ions_namd.str\n\nmgridforcechecksize 0 off/g" \
+mdff-step1.namd.BAK > mdff-step1.namd
 
 rm -f tmp.tcl tmp.dx
