e133241e2e273e62f335ed79f15fefdd15fda3b5
skblnw <i@skblnw.com>
2021-03-10 23:10:00 +0800

    Routine

--- NAMD/vmd_solvate/mkvmd_solvate.sh
+++ NAMD/vmd_solvate/mkvmd_solvate.sh
@@ -2,10 +2,10 @@
 source ~/.zshrc
 
 cat > tcl <<'EOF'
-set filename 6xra_charmmgui
+set filename prot
 package require solvate
 #solvate $filename.psf $filename.pdb -b 2.4 -minmax {{-77 -76 -35} {77 76 130}} -o solvated
-solvate $filename.psf $filename.pdb -b 2.4 -t 8 -o solvated
+solvate $filename.psf $filename.pdb -b 2.4 -t 10 -o solvated
 package require autoionize
 autoionize -psf solvated.psf -pdb solvated.pdb -sc 0.18 -cation POT -o ionized
 quit
