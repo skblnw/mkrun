0b3314b99e4c71518dad13fc49f4e7bb6d2a7fb9
kevin <i@skblnw.com>
2021-07-02 08:58:08 +0800

    Routine

--- NAMD/fep/mknamd_fep_run.sh
+++ NAMD/fep/mknamd_fep_run.sh
@@ -4,5 +4,5 @@ NAMD="namd3 +p1 +devices 1"
 
 for ii in $(seq 1 5); do
     rsync -avh eq/fep.soft.gpu.namd eq/equilibrate.coor eq/equilibrate.vel eq/equilibrate.xsc t$ii
-    $NAMD t$ii/fep.soft.namd >& t$ii/LOG_fep
+    $NAMD t$ii/fep.soft.gpu.namd >& t$ii/LOG_fep
 done
