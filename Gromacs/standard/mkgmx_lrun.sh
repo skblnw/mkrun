93cc47108e0564dbc94533d4248e2ecea4cddbdc
skblnw <i@skblnw.com>
2023-08-15 14:04:18 +0800

    Modified mkgmx_run

--- Gromacs/standard/mkgmx_lrun.sh
+++ Gromacs/standard/mkgmx_lrun.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+# Check if the number of arguments is zero
+if [ $# -eq 0 ]; then 
+    echo "mkvmd> Usage: $0 [GPU ID] [PREFIX] [PREVIOUS]"
+    echo "mkvmd> Suggested: 0 t1; [PREVIOUS] can be empty"
+    echo "mkvmd> OR Suggested: 0 t1 t1; [PREVIOUS] only contains the prefix"
+    exit 1
+fi
+
+# Define parameters
 GPUID="$1"
 PREFIX="$2"
-PREVIOIUS="$3"
+PREVIOUS="$3"
+TPR_FILE="md.tpr"
 MDRUN_GPU="gmx mdrun -ntmpi 1 -ntomp 16 -gpu_id $GPUID -pme gpu -nb gpu -bonded gpu -update gpu"
-[ $# -eq 0 ] && { echo "mkvmd> Usage: $0 [GPU ID] [PEFIX] [PREVIOIUS]; Suggested: 0 t1; [PREVIOIUS] can be empty"; exit 1; }
 
-[ ! -f "md.tpr" ] && echo -e "mkgmx> md.tpr not found!" && exit 1 
+# Check if the TPR file exists
+[ ! -f "$TPR_FILE" ] && echo "mkvmd> $TPR_FILE not found!" && exit 1 
 
-prefix=$PREFIX
-tpr=md.tpr
-
-if [ -z "$PREVIOIUS" ]; then
-    [ -f "${PREFIX}.cpt" ] && echo -e "mkgmx> ${PREFIX}.cpt found but you wanna start a new one." && exit 1 
-    echo "mkvmd> Start a new simulation"
-    $MDRUN_GPU -v -s ${tpr} -deffnm ${prefix} -nsteps -1
+# Start a new simulation or continue from previous checkpoint
+if [ -z "$PREVIOUS" ]; then
+    [ -f "${PREFIX}.cpt" ] && echo "mkvmd> ${PREFIX}.cpt found but you want to start a new one." && exit 1 
+    echo "mkvmd> Starting a new simulation"
+    $MDRUN_GPU -v -s $TPR_FILE -deffnm $PREFIX -nsteps -1
 else
-    [ ! -f "${PREVIOIUS}.cpt" ] && echo -e "mkgmx> ${PREVIOIUS}.cpt not found!" && exit 1 
-    cp ${PREVIOIUS}.cpt ${PREVIOIUS}.cpt.BAK
-    $MDRUN_GPU -v -s ${tpr} -cpi ${PREVIOIUS}.cpt -deffnm ${prefix} -nsteps -1
+    [ ! -f "${PREVIOUS}.cpt" ] && echo "mkvmd> ${PREVIOUS}.cpt not found!" && exit 1 
+    cp ${PREVIOUS}.cpt ${PREVIOUS}.cpt.BAK
+    echo "mkvmd> Continuing simulation from ${PREVIOUS}.cpt"
+    $MDRUN_GPU -v -s $TPR_FILE -cpi ${PREVIOUS}.cpt -deffnm $PREFIX -nsteps -1
 fi
 
 # gmx grompp -o md.tpr -f step7_production_vrescale.mdp -c step7_1.gro -t step7_1.cpt -n index.ndx -p topol.top
--- Gromacs/standard/mkgmx_run.sh
+++ Gromacs/standard/mkgmx_run.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# Define parameters and commands
 GPUID="$1"
 GMX="gmx"
 MDRUN="gmx mdrun -ntmpi 1 -ntomp 8 -gpu_id $GPUID"
 MDRUN_GPU="gmx mdrun -ntmpi 1 -ntomp 8 -gpu_id $GPUID -pme gpu -nb gpu -bonded gpu -update gpu"
-[ $# -eq 0 ] && { echo "mkgmx> Usage: $0 [GPU ID]; Suggested: 0"; exit 1; }
 
-[ ! -d pdb2gmx ] && echo -e "mkgmx> Directory pdb2gmx not found!" && exit 1 
+# Check if the number of arguments is zero
+if [ $# -eq 0 ]; then 
+    echo "mkgmx> Usage: $0 [GPU ID]; Suggested: 0"
+    exit 1
+fi
 
-files=("pdb2gmx/ionized.gro" "pdb2gmx/topol.top" "pdb2gmx/index.ndx" )
+# Check if the required directory exists
+[ ! -d pdb2gmx ] && echo "mkgmx> Directory pdb2gmx not found!" && exit 1 
+
+# Check if the required files exist
+files=("pdb2gmx/ionized.gro" "pdb2gmx/topol.top" "pdb2gmx/index.ndx")
 for file in "${files[@]}"; do
-    if [ ! -f "$file" ]; then
-        echo "mkgmx> $file not found!"
-        exit 1
-    fi
+    [ ! -f "$file" ] && echo "mkgmx> $file not found!" && exit 1
 done
 
+# Simulation settings
 mkdir -p output
 mini=true
 double=false
@@ -28,55 +34,30 @@ INITIAL_PDB=pdb2gmx/ionized.gro
 NDX=pdb2gmx/index.ndx
 TOP=pdb2gmx/topol.top
 
-if $mini; then
-    prefix=step1_mini
-    TPR=${prefix}.tpr
+# Function to run a simulation step
+run_simulation() {
+    local previous="$1"
+    local prefix="$2"
+    local mdp_file="$3"
+    local TPR="${prefix}.tpr"
     rm -f $TPR
-    $GMX grompp -f mdp/step1_mini.mdp -o $TPR -c $INITIAL_PDB -r $INITIAL_PDB -n $NDX -p $TOP
-    $MDRUN -v -s $TPR -deffnm output/${prefix}
-fi
-    
-if $double; then
-    previous=step1_mini
-    prefix=step1_mini_double
-    TPR=${prefix}.tpr
-    rm -f $TPR
-    $GMX grompp -f mdp/step1_mini_double.mdp -o $TPR -c output/${previous}.gro -r $INITIAL_PDB -n $NDX -p $TOP
-    gmx_d mdrun -ntmpi 1 -ntomp 12 -s $TPR -v -deffnm output/${prefix}
-fi
+    $GMX grompp -f $mdp_file -o $TPR -c output/${previous}.gro -r $INITIAL_PDB -n $NDX -p $TOP
+    $4 -v -s $TPR -deffnm output/${prefix}
+}
 
-if $heat; then
-    previous=step1_mini
-    prefix=step3_annealing
-    TPR=${prefix}.tpr
-    rm -f $TPR
-    $GMX grompp -f mdp/step3_annealing.mdp -o $TPR -c output/${previous}.gro -r $INITIAL_PDB -n $NDX -p $TOP
-    $MDRUN -v -s $TPR -deffnm output/${prefix}
-fi
-
-if $eq_npt; then
-    previous=step3_annealing
-    prefix=step4_eq_npt
-    TPR=${prefix}.tpr
-    rm -f $TPR
-    $GMX grompp -f mdp/step4_eq_npt.mdp -o $TPR -c output/${previous}.gro -r $INITIAL_PDB -n $NDX -p $TOP
-    $MDRUN -v -s $TPR -deffnm output/${prefix}
-fi
-
-if $md1; then
-    previous=step4_eq_npt
-    prefix=md
-    TPR=${prefix}.tpr
-    rm -f $TPR
-    $GMX grompp -f mdp/step5_md.mdp -o $TPR -c output/${previous}.gro -r $INITIAL_PDB -n $NDX -p $TOP
-    $MDRUN_GPU -v -s $TPR -deffnm output/${prefix}
-fi
+# Simulation steps based on the set conditions
+$mini && run_simulation "$INITIAL_PDB" "step1_mini" "mdp/step1_mini.mdp" "$MDRUN"
+$double && run_simulation "step1_mini" "step1_mini_double" "mdp/step1_mini_double.mdp" "gmx_d mdrun -ntmpi 1 -ntomp 8"
+$heat && run_simulation "step1_mini" "step3_annealing" "mdp/step3_annealing.mdp" "$MDRUN"
+$eq_npt && run_simulation "step3_annealing" "step4_eq_npt" "mdp/step4_eq_npt.mdp" "$MDRUN"
+$md1 && run_simulation "step4_eq_npt" "md" "mdp/step5_md.mdp" "$MDRUN_GPU"
 
 if $md2; then
-    previous=md
-    prefix=t1
-    TPR=md.tpr
+    previous="md"
+    prefix="t1"
+    TPR="md.tpr"
     $GMX grompp -f step5_md.mdp -o $TPR -t output/${previous}.cpt -c $INITIAL_PDB -r $INITIAL_PDB -n $NDX -p $TOP
+    # Uncomment the lines below if needed
     # gmx convert-tpr -s ${previous}.tpr -o $prefix.tpr -extend 10000
     # $MDRUN_GPU -v -s $TPR -cpi output/${prefix}.cpt -deffnm output/${prefix} -nsteps -1
     # $MDRUN_GPU -v -s $TPR -deffnm output/${prefix} -nsteps -1
