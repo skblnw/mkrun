1d2a1037d96443e2853fb67e2c801da54369ac45
skblnw <i@skblnw.com>
2023-08-21 14:58:26 +0800

    modified assign_chain_for_gro.py

--- tools/pdb/assign_chain_for_gro.py
+++ tools/pdb/assign_chain_for_gro.py
@@ -1,71 +1,52 @@
-#!/usr/bin/env python3
-"""
-assign_chain_ids.py
-
-Usage: python assign_chain_ids.py [filename]
-
-This script reads a PDB file, identifies different molecules by finding breaks in residue IDs, 
-and assigns chain IDs to them. It first assigns 'A', 'B', and 'C' to the first three separate 
-molecules and 'X' to all the rest. A 'break' is identified when the residue ID is not the same 
-or not one greater than the previous residue ID.
-
-Prerequisites:
-1. Python 3.x
-2. A valid PDB file with properly sorted atoms. The script assumes that each ATOM line of the PDB 
-file has the residue ID at columns 23-26 (1-based counting), and the chain ID at column 22. Please 
-ensure that the ATOM lines are sorted by residue ID.
-
-This script does not require any external Python libraries and uses only Python's built-in modules.
-
-Written by: OpenAI's ChatGPT-4
-"""
-
 import sys
+import argparse
 
-def assign_chain_ids(filename):
-    # Open the PDB file
+def assign_chain_ids(filename, chains, starts):
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-    # Define variable to hold previous residue id, chain counter, and a list for the new file lines
     prev_res_id = None
     chain_counter = 0
-    chains = ['A', 'B', 'C']
     new_lines = []
 
     for line in lines:
-        # Only consider lines starting with 'ATOM'
         if line.startswith('ATOM'):
-            # Extract residue id from the line
             res_id = int(line[22:26].strip())
 
-            # If there is a break in the sequence of residue ids or the residue id decreases, a new molecule starts
             if prev_res_id is not None and (res_id != prev_res_id and res_id != prev_res_id + 1):
                 chain_counter += 1
 
-            # Assign the chain id
             chain_id = chains[chain_counter] if chain_counter < len(chains) else 'X'
+            resid_offset = starts[chain_counter]
 
-            # Update the line with the new chain id
-            new_line = line[:21] + chain_id + line[22:]
+            # Adjust the residue ID by subtracting the initial residue id of the previous chain and adding the starting residue id of the current chain
+            new_res_id = res_id + resid_offset - (prev_res_id if prev_res_id is not None else 0)
+
+            # Update the line with the new chain id and the new residue ID
+            new_line = line[:21] + chain_id + f"{new_res_id:>4}" + line[26:]
             new_lines.append(new_line)
 
-            # Update the previous residue id
             prev_res_id = res_id
 
-        # If the line does not start with 'ATOM', it is copied without changes
         else:
             new_lines.append(line)
 
-    # Write the new file
     with open('new_' + filename, 'w') as f:
         f.writelines(new_lines)
 
-# Get the filename from the command line arguments
-if len(sys.argv) != 2:
-    print("Usage: python script.py <input_PDB_file>")
-    exit(1)
-filename = sys.argv[1]
+def main():
+    parser = argparse.ArgumentParser(description="Assign specified chain IDs and start residue numbers to a PDB file.")
+    parser.add_argument("pdb_file", type=str, help="Path to the input PDB file.")
+    parser.add_argument("--chains", type=str, nargs='+', help="List of chain IDs to assign.", required=True)
+    parser.add_argument("--start", type=int, nargs='+', help="List of starting residue numbers for each chain.", required=True)
 
-# Call the function with your filename
-assign_chain_ids(filename)
+    args = parser.parse_args()
+
+    if len(args.chains) != len(args.start):
+        print("Error: The number of chains should match the number of starting residue numbers.")
+        exit(1)
+
+    assign_chain_ids(args.pdb_file, args.chains, args.start)
+
+if __name__ == "__main__":
+    main()
