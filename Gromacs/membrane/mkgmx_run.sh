e3971e673721d3af2aa44b861ff388c6a5473c26
kevin <i@skblnw.com>
2023-08-31 20:33:51 +0800

    Routine

--- Gromacs/membrane/mkgmx_run.sh
+++ Gromacs/membrane/mkgmx_run.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
 # Define parameters and commands
+GPUID="$1"
 GMX="gmx"
+MDRUN="gmx mdrun -ntmpi 1 -ntomp 8 -gpu_id $GPUID"
+MDRUN_GPU="gmx mdrun -ntmpi 1 -ntomp 8 -gpu_id $GPUID -pme gpu -nb gpu -bonded gpu -update gpu"
+
+# Check if the number of arguments is zero
+if [ $# -eq 0 ]; then 
+    echo "mkgmx> Usage: $0 [GPU ID]; Suggested: 0"
+    exit 1
+fi
+
+# Check if the required directory exists
+[ ! -d pdb2gmx ] && echo "mkgmx> Directory pdb2gmx not found!" && exit 1 
+
+# Check if the required files exist
+files=("pdb2gmx/ionized.pdb" "pdb2gmx/topol.top" "pdb2gmx/index.ndx")
+for file in "${files[@]}"; do
+    [ ! -f "$file" ] && echo "mkgmx> $file not found!" && exit 1
+done
+
+# Simulation settings
+mini=false
+eq=true
+md=false
+
+TOP="pdb2gmx/topol.top"
+NDX="pdb2gmx/index.ndx"
+INITIAL_PDB="pdb2gmx/ionized.pdb"
 
 # Minimization
 mini_prefix="step6.0_minimization"
-init="step5_input"
-rest_prefix="step5_input"
 
 run_minimization() {
     local prefix="$1"
-    local init_gro="$2"
-    local rest_gro="$3"
+    local init="$2"
     
     # Preprocess the input
-    $GMX grompp -f ${prefix}.mdp -o ${prefix}.tpr -c ${init_gro}.gro -r ${rest_gro}.gro -p topol.top -n index.ndx
+    $GMX grompp -f mdp/${prefix}.mdp -o ${prefix}.tpr -c ${INITIAL_PDB} -r ${INITIAL_PDB} -p ${TOP} -n ${NDX}
 
     # Run the minimization
-    gmx_d mdrun -v -deffnm ${prefix}
+    $MDRUN -v -deffnm ${prefix}
 }
 
-run_minimization $mini_prefix $init $rest_prefix
+$mini && run_minimization $mini_prefix
 
 # Equilibration
 equi_prefix="step6.%d_equilibration"
@@ -29,9 +53,7 @@ cntmax=6
 
 run_equilibration() {
     local prefix="$1"
-    local previous="$2"
-    local rest_gro="$3"
-    local count="$4"
+    local count="$2"
     
     istep=$(printf ${prefix} ${count})
     pstep=$(printf ${prefix} $((${count}-1)))
@@ -41,14 +63,13 @@ run_equilibration() {
     fi
 
     # Preprocess the input
-    $GMX grompp -f ${istep}.mdp -o ${istep}.tpr -c ${pstep}.gro -r ${rest_gro}.gro -p topol.top -n index.ndx
-
+    $GMX grompp -f mdp/${istep}.mdp -o ${istep}.tpr -c ${pstep}.gro -r ${INITIAL_PDB} -p ${TOP} -n ${NDX}
     # Run the equilibration
-    $GMX mdrun -v -deffnm ${istep}
+    $MDRUN -v -deffnm ${istep}
 }
 
 while [ $cnt -le $cntmax ]; do
-    run_equilibration $equi_prefix $mini_prefix $rest_prefix $cnt
+    $eq && run_equilibration $equi_prefix $cnt
     cnt=$((cnt+1))
 done
 
@@ -69,16 +90,16 @@ run_production() {
     
     if [ ${count} -eq 1 ]; then
         pstep=$(printf ${equi_prefix} 6)
-        $GMX grompp -f ${prefix}.mdp -o ${istep}.tpr -c ${pstep}.gro -p topol.top -n index.ndx
+        $GMX grompp -f ${prefix}.mdp -o ${istep}.tpr -c ${pstep}.gro -p ${TOP} -n ${NDX}
     else
-        $GMX grompp -f ${prefix}.mdp -o ${istep}.tpr -c ${pstep}.gro -t ${pstep}.cpt -p topol.top -n index.ndx
+        $GMX grompp -f ${prefix}.mdp -o ${istep}.tpr -c ${pstep}.gro -t ${pstep}.cpt -p ${TOP} -n ${NDX}
     fi
 
     # Run the production
-    $GMX mdrun -v -deffnm ${istep}
+    $MDRUN_GPU -v -deffnm ${istep}
 }
 
 while [ $cnt -le $cntmax ]; do
-    run_production $prod_step $equi_prefix $prod_prefix $cnt
+    $md && run_production $prod_step $equi_prefix $prod_prefix $cnt
     cnt=$((cnt+1))
 done
