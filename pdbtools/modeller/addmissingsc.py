1295c906fdd39e1ca241dadb8b2c93d493e0dd5d
kevin <i@skblnw.com>
2024-03-18 14:57:55 +0800

    Routine

--- pdbtools/modeller/addmissingsc.py
+++ pdbtools/modeller/addmissingsc.py
@@ -4,7 +4,7 @@ from modeller.automodel import *
 
 # Check if a PDB file was provided as an argument
 if len(sys.argv) != 2:
-    print("Usage: python this_script.py your_pdb_file.pdb")
+    print("Usage: python addmissingsc.py pdb_file.pdb")
     sys.exit(1)
 
 pdb_file = sys.argv[1]
@@ -17,38 +17,35 @@ log.verbose()
 env = environ()
 env.io.atom_files_directory = ['.', '/path/to/your/pdb/files']
 mdl = model(env, file=pdb_file)
-
-# Write the sequence to a file
-mdl.write(sequence=sequence_file)
+aln = alignment(env)
+aln.append_model(mdl, align_codes=pdb_file)
+aln.write(file=sequence_file)
 
 # Step 2: Create an alignment file by duplicating the sequence
 with open(sequence_file) as seq_file:
-    sequence = seq_file.read()
+    lines = seq_file.readlines()
+    sequence = ''.join(lines[2:])
 
 with open(alignment_file, 'w') as ali_file:
     ali_file.write(f">P1;{pdb_code}\n")
-    ali_file.write("sequence:::::::::\n")
     ali_file.write(sequence)
-    ali_file.write(f"*{pdb_code}:::::::0.00: 0.00\n")
+    ali_file.write(f">P1;{pdb_code}_fill\n")
     ali_file.write(sequence)
-    ali_file.write("*\n")
 
 # Step 3: Load the structure, identify missing side chains, and add them
 class AddMissingSideChains(automodel):
     def select_atoms(self):
         # Here, we're returning all atoms, assuming we want to check the entire structure
         # for missing side chains. Adjust if needed.
-        return selection(self.residue_range('1:', f'{mdl.last_residue.num}'))
-
-log.verbose()
-env = environ()
+        return selection(self)
 
 # Load the alignment file and known structure
 a = alignment(env, file=alignment_file)
 m = AddMissingSideChains(env, alnfile=alignment_file,
-                         knowns=pdb_code, sequence=pdb_code)
+                         knowns=pdb_code, sequence=pdb_code, assess_methods=(assess.DOPE,assess.GA341))
 m.starting_model = 1
 m.ending_model = 1
 
 # Perform the task
+m.very_fast()
 m.make()
